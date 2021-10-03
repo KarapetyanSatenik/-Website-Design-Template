21869783419b2b61457af65002cb43bbe015a792
karapetyansatenik <karapetyansatenik99@gmail.com>
2021-10-03 23:42:35 +0400

    DOCS

--- README.md
+++ README.md
@@ -4,7 +4,3 @@
 #### ABOUT TEAM
 We can help you reimagine your business through a digital lens. Our software engineering heritage combined with our strategic business and innovation consulting, design thinking, and physical-digital capabilities provide real business value to our customers through human-centric innovation.
 
-
-
-#### OUR RESULTS
- Our teams of technologists, strategists and designers deliver powerful digital experiences. We specialize in 11 industries in 35 countries, delivering innovative solutions to our customers’ most challenging problems. The numbers speak for themselves.
--- functions/main.js
+++ functions/main.js
@@ -1,25 +1,4 @@
-const btn = document.querySelector('.btn-for-click')
-btn.addEventListener('click', () => {
-    window.history.pushState('', '', window.location + '/hello')
-})
-
-class User {
-    constructor(name) {
-        this.name = name;
-    }
-}
-
-class User {
-    constructor(name) {
-        this.name = name;
-
-    }
-}
-class Person extends User {
-    constructor(name, age) {
-        super(name)
-        this.age = age
-    }
-}
-let p1 = new Person('Sat', 22)
-console.log(p1);
+// const btn = document.querySelector('.btn-for-click')
+// btn.addEventListener('click', () => {
+//     window.history.pushState('', '', window.location + '/hello')
+// })
